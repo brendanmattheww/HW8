db4589768a9b446bb80b89434debf7b948195ebc
zrg314 <zgregory@cedarville.edu>
2023-04-03 16:16:29 -0400

    Probably finished Wire.cpp and wire.h

--- Wire.h
+++ Wire.h
@@ -1,10 +1,19 @@
 #pragma once
+using namespace std;
+#include <string>
+#include <vector>
 class Gate;
 
 class Wire {
 public:
-
+	Wire(int v, string h);
+	int SetValue(int v);
+	string setHistory(string h);
+	Gate* setDrives()
 private:
-
-
+	int value;
+	string name;
+	vector<Gate*> drives;
+	int index;		 //wire number
+	string history;
 };
