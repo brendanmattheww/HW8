230685d51edde69d2a657142047291fe30cbbc42
zrg314 <zgregory@cedarville.edu>
2023-04-03 15:37:37 -0400

    Added Gate.cpp and Gate.h

---
+++ Gate.cpp
@@ -0,0 +1,9 @@
+#include "Gate.h"
+
+Gate::Gate(string t, int d, Wire* i1, Wire* i2, Wire* o) {
+	type = t;
+	delay = d;
+	in1 = i1;
+	in2 = i2;
+	out = o;
+}
---
+++ Gate.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+class Wire;
+
+class Gate {
+public:
+	Gate(string t, int d, Wire* i1, Wire* i2, Wire* o);
+	string getType() const { return type; }
+private:
+	string type;
+	int delay;
+	Wire* in1;
+	Wire* in2;
+	Wire* out;
+
+
+};
