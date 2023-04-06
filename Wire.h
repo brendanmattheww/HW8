0c2b70a4539a539297df761176e87d3f32094d0e
brendanmattheww <brendanthomas@cedarville.edu>
2023-04-06 08:57:26 -0400

    Made the history attribute a vector<int> and added int v to setHistory. Made a "rough draft" of the printHistory function, still needs work.

--- Wire.h
+++ Wire.h
@@ -6,24 +6,24 @@ class Gate;
 
 class Wire {
 public:
-	Wire(int v, string h);
+	Wire(int v, vector<int> h);
 	void SetValue(int v);
-	void setHistory(string h);
+	void setHistory(int v, vector<int> h);
 	void addGate(Gate* i);
 
 	int GetValue() const		   { return value;   }
 	string GetName() const		   { return name;    }
 	vector<Gate*> GetGate() const  { return drives;  }
 	int GetIndex() const		   { return index;   }
-	string GetHistory() const	   { return history; }
+	vector<int> GetHistory() const { return history; }
 
 	void printHistory() const;
 
 private:
-	int value;			  // // 1 = high;  0 = low;  -1 = unknown
-	string name;		  // ABC
-	vector<Gate*> drives; // gate
-	int index;			  // wire number
-	string history;		  // history of values (--__xx)
+	int value;				// 1 = high;  0 = low;  -1 = unknown
+	string name;			// ABC
+	vector<Gate*> drives;	// gate
+	int index;				// wire number
+	vector<int> history;  // history of values (--__xx)
 
 };
