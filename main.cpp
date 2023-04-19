db4dde35aa341841ec690158d17f54d583201455
zrg314 <zgregory@cedarville.edu>
2023-04-19 19:22:50 -0400

    pulling

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@
 using namespace std;
 
 int main(){
+	string fileName = "";
 	ofstream file;
-	file.open();
+	file.open(fileName);
+	while(!file.eof()) {
+		
+	}
 	
 	//readCircuitDesc(circDesc, gates, wires);    Shomper showed this in his main.cpp file
 	//readVectorDesc(vecDesc, wires, q);			in class
