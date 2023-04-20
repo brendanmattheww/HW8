b2c2d16dcccbe599c3a7814b3d4b87f154e60443
zrg314 <zgregory@cedarville.edu>
2023-04-20 14:55:01 -0400

    push

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+//#include <ostream>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -16,6 +17,7 @@ int main() {
 	string fileName = "";
 	vector<Wire*> vecWires;
 	vector<Gate*> vecGates;
+	vector<Wire*> tempWires;
 	vector<int> wireInts;
 		vecWires.push_back(nullptr);
 	
@@ -30,14 +32,21 @@ int main() {
 	else {
 		cout << "file opened" << endl;
 	}
+
+	getline(circuitFile, currLine);
 	while(!circuitFile.eof()) {
 		getline(circuitFile, currLine);
-		string firstWord = currLine.substr(0, currLine.find("")); 
+		string firstWord = currLine.substr(0, currLine.find(" ")); 
 		
-		if (firstWord == "INPUT" || firstWord == "OUTPUT") {
+		
+		if ((firstWord == "INPUT") || (firstWord == "OUTPUT")) {
+			
 			stringstream ss(currLine);
+			string firstWord = "";
 			string wireName = "";
 			string wireNumber = "";
+			
+			ss >> firstWord;
 			ss >> wireName;
 			ss >> wireNumber;
 			Wire inputWire(-1, wireName, stoi(wireNumber));
@@ -50,28 +59,37 @@ int main() {
 			}
 			vecWires.push_back(&inputWire); // took out the line where inpw = &inputWire and just did it in the push_back
 		}
-
+		
 		else if (firstWord != "CIRCUIT") {
 			stringstream ss(currLine);
 			string gateDelay = "";
 			string wire = "";
-			int wn = 0;
+			int wireNum = 0;
+			string firstWord = "";
+			//int wn = 0;
 			int delay = 0;
-			
+			ss >> firstWord;
 			ss >> gateDelay;
-			ss >> wire;
-			wn = stoi(wire);
-			wireInts.push_back(wn);
+			for (int i = 0; i < 3; i++) {
+				ss >> wire;
+				wireNum = stoi(wire);
+				wireInts.push_back(wireNum);
+				Wire* currWire = vecWires.at(wireInts.at(wireNum - 1));
+				tempWires.push_back(currWire);
+			}
 			string gateType = firstWord;
 			delay = stoi(gateDelay.substr(0, gateDelay.length() - 2));
-
-			Wire* Wire1 = vecWires.at(wireInts.at(0));
-			Wire* Wire2 = vecWires.at(wireInts.at(1));
-			Wire* Wire3 = vecWires.at(wireInts.at(3));
-			Gate currGate(gateType, delay, Wire1, Wire2, Wire3);
-			vecGates.push_back(&currGate);	// puts the gates into the vector
-			Wire1->addGate(&currGate);		// Tells the wires which gate they are driving
-			Wire2->addGate(&currGate);
+			
+			
+			
+			if (tempWires.size() == 3) {   // need to check for NOT gate
+				Gate currGate(gateType, delay, tempWires.at(0), tempWires.at(1), tempWires.at(2));
+				vecGates.push_back(&currGate);
+				tempWires.at(0)->addGate(&currGate);		// Tells the wires which gate they are driving
+				tempWires.at(1)->addGate(&currGate);
+			}
+										// puts the gates into the vector
+			
 		}
 		
 
