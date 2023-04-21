a6c4787195cb30c0e1008a4c1fd8e07e681f26af
zrg314 <zgregory@cedarville.edu>
2023-04-21 16:50:18 -0400

    making it work for NOT gates

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <ostream>
+#//include <ostream>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -19,9 +19,11 @@ int main() {
 	vector<Wire*> vecWires;
 	vector<Gate*> vecGates;
 	vector<Wire*> tempWires;
-	vector<int> wireInts;
 		vecWires.push_back(nullptr);
 
+	Wire* wire1;
+	Wire* wire2;
+	Wire* wire3;
 	//For everything
 	string firstWord = "";
 	int wireNum = 0;
@@ -71,27 +73,53 @@ int main() {
 		
 		else if (firstWord != "CIRCUIT") {
 			string gateType = firstWord;
+			vector<int> wireInts;
+			wireInts.push_back(0);
 
 			ss >> gateDelay;
 			delay = stoi(gateDelay.substr(0, gateDelay.length() - 2));
+			
+			int numOfWires = 0;
+			if (gateType == "NOT") {
+				numOfWires = 2;
+			}
+			else {
+				numOfWires = 3;
+			}
 
-			for (int i = 0; i < 3; i++) {		// puts the three wires into a vector so they can be used to initialize the gate
+			for (int i = 0; i < numOfWires; i++) {		// puts the three wires into a vector so they can be used to initialize the gate
 				ss >> wire;
 				wireNum = stoi(wire);
 				wireInts.push_back(wireNum);
-				Wire* currWire = vecWires.at(wireInts.at(wireNum - 1));
-				tempWires.push_back(currWire);				
+				//Wire* currWire = vecWires.at(wireInts.at(wireNum - 1));
+				//tempWires.push_back(currWire);				
 			}
-			
-			if (tempWires.size() == 3) {					// need to check for NOT gate
-				Gate currGate(gateType, delay, tempWires.at(0), tempWires.at(1), tempWires.at(2));
-				vecGates.push_back(&currGate);
-				tempWires.at(0)->addGate(&currGate);		// Tells the wires which gate they are driving
-				tempWires.at(1)->addGate(&currGate);
+			for (int i = 1; i < wireInts.size(); i++) {
+				if (vecWires.at(wireInts.at(i)) == nullptr) {
+					Wire* newWire = new Wire(-1, "", wireInts.at(i));
+					vecWires.at(i) = newWire;
+				}
+			}
+			wire1 = vecWires.at(wireInts.at(1));
+			if (numOfWires == 3) {
+				wire2 = vecWires.at(wireInts.at(2));
+				wire3 = vecWires.at(wireInts.at(3));
+			}
+			else {
+				wire2 = nullptr;
+				wire3 = vecWires.at(wireInts.at(2));
+			}
+			Gate* currGate = new Gate(gateType, delay, wire1, wire2, wire3);
+			vecGates.push_back(currGate);
+			wire1->addGate(currGate);	// Tells the wires which gate they are 
+			if (numOfWires == 3) {
+				wire2->addGate(currGate);	// not adding the second gate for a wire correctly
 			}
 		}
 	}
+	
 	circuitFile.close();
+	vectorFile.close();
 	cout << "file closed" << endl;
 
 	//readCircuitDesc(circDesc, gates, wires);    Shomper showed this in his main.cpp file
